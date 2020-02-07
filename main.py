8aa9b000496c0787e7d43746e820839166692744
= <=>
2020-02-07 14:00:08 +0900

    update

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ from data import load_data
 if __name__=='__main__':
     data = load_data('cora')
     model, optimizer = create_gcn_model(data)
-    run(data, model, optimizer, niter=10)
+    run(data, model, optimizer, niter=10, verbose=True)
